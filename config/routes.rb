e332d8cc826e69ad9bfbe02244672d5006e7f889
Jim Kyle <jim@jwkyle.com>
2023-08-12 22:19:14 -0500

    edit route for bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'calculations#new'
-  post 'multiply' to: 'calculations#new'
+  post 'multiply', to: 'calculations#new'
   # get 'calculations/new'
   # get 'calculations/multiply'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
