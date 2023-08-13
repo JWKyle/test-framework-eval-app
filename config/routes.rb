d9c4bff0a39e50ff82f621b043878fd7a1af5b96
Jim Kyle <jim@jwkyle.com>
2023-08-12 22:17:38 -0500

    add route for multiply new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'calculations/new'
-  get 'calculations/multiply'
+  root 'calculations#new'
+  post 'multiply' to: 'calculations#new'
+  # get 'calculations/new'
+  # get 'calculations/multiply'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
