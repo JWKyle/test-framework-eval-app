86acff6f7d721e9cdb29dddeaee8e0cce3ee5422
Jim Kyle <jim@jwkyle.com>
2023-08-12 22:29:36 -0500

    add initial multiplication logic

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,5 +3,9 @@ class CalculationsController < ApplicationController
   end
 
   def multiply
+    @number1 = params[:number1].to_f
+    @number2 = params[:number2].to_f
+    @result = @number1 * @number2
+    render :multiply
   end
 end
